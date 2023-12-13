f4d4ca13027c01e12ecc88df6427f4567979b1fd
Luiz César Pacheco <lcpsantos@gmail.com>
2023-12-12 23:36:28 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,27 +1,14 @@
 package chess;
 
+import boardgame.BoardException;
+
 import java.io.Serial;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
     @Serial
     private static final long serialVersionUID = 1L;
 
     public ChessException(String msg) {
         super(msg);
-    }
-
-    public ChessException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-
-    public ChessException(Throwable cause) {
-        super(cause);
-    }
-
-    public ChessException() {
-    }
-
-    public ChessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
